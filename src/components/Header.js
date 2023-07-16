85bc6e10bb9939b90c6535b349eb7a6fc2d258b6
Serhii <sergenadoenko@gmail.com>
2023-07-16 14:40:25 +0300

    second step

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,7 +27,7 @@ const Header = () => {
           <div className="row align-items-center">
             <div className="col-2">
               <h2>
-                <Link className="text-white">Shoppers</Link>
+                <Link className="text-white spacing">Shoppers</Link>
               </h2>
             </div>
             <div className="col-5">
@@ -87,8 +87,59 @@ const Header = () => {
           </div>
         </div>
       </header>
+      <header className="header-bottom py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="menu-bottom d-flex align-items-center gap-30">
+                <div>
+                  <div className="dropdown">
+                    <button
+                      className="btn btn-secondary dropdown-toggle bg-transparent border-0 d-flex gap-15 align-items-center"
+                      type="button"
+                      id="dropdownMenuButton1"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false">
+                      <img src="/images/menu.svg" alt="menu" />
+                      <span className="me-5 d-inline-block">Shop Categories</span>
+                    </button>
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                      <li>
+                        <Link to="/popa" className="dropdown-item text-white">
+                          Action
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/" className="dropdown-item text-white">
+                          Another action
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/" className="dropdown-item text-white">
+                          Something else here
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="menu-links">
+                  <div className="d-flex align-items-center"></div>
+                  <div className="d-flex align-items-center gap-15">
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/">Our Store</NavLink>
+                    <NavLink to="/">Blogs</NavLink>
+                    <NavLink to="/contact">Contacs</NavLink>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
     </>
   );
 };
 
 export default Header;
+
+/* 49.39 */
