de8a78a5ea1878b638c02efe9c112a0b048f121c
Serhii <sergenadoenko@gmail.com>
2023-07-20 20:24:38 +0200

    6th step

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import Marquee from 'react-fast-marquee';
+import BlogCard from '../components/BlogCard';
 const Home = () => {
   return (
     <>
@@ -194,6 +195,52 @@ const Home = () => {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+      <section className="marquee-wrapper py-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="marquee-inner-wrapper card-wrapper">
+                <Marquee className="d-flex">
+                  <div className="mx-5">
+                    <img src="images/brand-01.png" alt="brand" />
+                  </div>
+                  <div className="mx-5">
+                    <img src="images/brand-02.png" alt="brand" />
+                  </div>
+                  <div className="mx-5">
+                    <img src="images/brand-03.png" alt="brand" />
+                  </div>
+                  <div className="mx-5">
+                    <img src="images/brand-04.png" alt="brand" />
+                  </div>
+                  <div className="mx-5">
+                    <img src="images/brand-05.png" alt="brand" />
+                  </div>
+                  <div className="mx-5">
+                    <img src="images/brand-06.png" alt="brand" />
+                  </div>
+                  <div className="mx-5">
+                    <img src="images/brand-07.png" alt="brand" />
+                  </div>
+                  <div className="mx-5">
+                    <img src="images/brand-08.png" alt="brand" />
+                  </div>
+                </Marquee>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="blog-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
           </div>
         </div>
       </section>
